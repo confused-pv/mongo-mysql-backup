23ca2f286dcc05ab57641b7a4348b1ade3b08b57
Prakhar Verma <prakhar007@gmail.com>
2015-05-06 17:47:00 -0700

    v1.0.0

---
+++ README.md
@@ -0,0 +1,44 @@
+# MongoDb / Mysql backup to S3
+
+A simple script to store back up in S3. It is ideal for medium scale backups. Mysqldump will lock take lock on all tables while generating .sql file. Hence it is not recommended for large databases. 
+Snapshot would be ideal for large scale databases.
+
+# Setup requirements
+  - aws cli should already be configured
+  - mongo shell if mongodb backup is required
+  - mysql-client if mysql backup is required
+ 
+# Configuration Files
+
+> config.ini
+  - TOTAL_BACKUPS : The number of host back up is required for.
+  - BACKUP_{$i} : If you need to take back for 3 host then, as many variables should be present. Explained below :
+
+```config.ini
+  TOTAL_BACKUPS=3
+  BACKUP_1=mongo_staging.ini
+  BACKUP_2=mongo_production.ini
+  BACKUP_3=mysql_production.ini
+```
+  
+> Config files for each host.
+- Each config files would be present under the config folder. It needs following variable
+
+```config.ini
+TYPE=[MONGO/MYSQL]
+DESTINATION_FOLDER=mongo_production
+HOST=mongo-prod-host
+PORT=27017
+USERNAME=confused-pv
+PASSWORD=YES
+DB=pv
+S3_BUCKET=mongo-production
+```
+# Execution
+
+> confused@Prakhars-MacBook-Pro ~ $ sh start_backup.sh
+
+You can just put in crontab of your system to make sure backup is done every day without any issue.
+
+### Version
+3.0.2
---
+++ start_backup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Backup mongo databases
+
+# stop on errors
+set -e
+
+pidDir=backup.pid
+# lock it
+if mkdir ./${pidDir}; then
+	echo "Locking succeeded" >&2
+else
+	echo "Lock failed - exit" >&2
+	exit 1
+fi
+
+rmdir ./${pidDir}
+exit;
+if [ -n "$1" ]; then
+    DATESTRING=$1
+else
+    DATESTRING=`date +"%Y-%m-%d"`
+fi
+
+source config.ini
+echo "Total number of back ups to be taken = "${TOTAL_BACKUPS}
+for i in `seq 1 ${TOTAL_BACKUPS}`;
+do
+	BACKUP_FILE=BACKUP_${i}
+	echo "sourcing backup file = "${BACKUP_FILE}
+	source ./config/${!BACKUP_FILE}
+	echo "file sourced = "${!BACKUP_FILE}
+	case "$TYPE" in
+
+		MONGO )
+			echo "Type of database found = MONGO";
+			echo "Creating destination folder - "${DESTINATION_FOLDER}
+			mkdir -p ${DESTINATION_FOLDER}
+			echo "Running mongo dump"
+			mongodump --host ${HOST} --port ${PORT} --username ${USERNAME} --password ${PASSWORD} --db ${DB} --out ./${DESTINATION_FOLDER}/backup-${DATESTRING}
+
+			echo "Compressing back folder"
+			tar -zcvf ./${DESTINATION_FOLDER}/backup-${DATESTRING}.tar.gz ./${DESTINATION_FOLDER}/backup-${DATESTRING}
+			echo "Removing uncompressed folder"
+			rm -rf ./${DESTINATION_FOLDER}/backup-${DATESTRING}
+
+			echo "Pushing Backup to S3 Bucket - "${S3_BUCKET}
+			aws s3api put-object --bucket ${S3_BUCKET} --key backup-${DATESTRING}.tar.gz --body ./${DESTINATION_FOLDER}/backup-${DATESTRING}.tar.gz
+
+			;;
+		MYSQL )
+			echo "Type of database found = MYSQL";
+			echo "Creating destination folder - "${DESTINATION_FOLDER}
+			mkdir -p ${DESTINATION_FOLDER}
+			echo "Running mysql dump"
+			mysqldump -u${USERNAME} -p${PASSWORD} -h${HOST} -P${PORT} ${DB} > ./${DESTINATION_FOLDER}/backup-${DATESTRING}.sql
+			echo "Compressing mysql file"
+			gzip ./${DESTINATION_FOLDER}/backup-${DATESTRING}.sql
+
+			echo "Pushing Backup to S3 Bucket - "${S3_BUCKET}
+			aws s3api put-object --bucket ${S3_BUCKET} --key backup-${DATESTRING}.sql.gz --body ./${DESTINATION_FOLDER}/backup-${DATESTRING}.sql.gz
+
+			;;
+	esac
+done
+
+mail -s "backup Completed" prakhar007@gmail.com <<< "Back Up Completed"
+rmdir ./${pidDir}
